ef2d2c600b3261b5ebbfe040b8d7d91f0efb1238
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-07-02 09:15:20 -0600

    Separate fullname into firstname and lastname on user model controller and schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,8 @@ ActiveRecord::Schema.define(version: 2021_07_01_145659) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "fullname"
+    t.string "firstname"
+    t.string "lastname"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
