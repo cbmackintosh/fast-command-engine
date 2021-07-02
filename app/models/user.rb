a3ac748f11e30e1c6641a8524f4cbe588d618dba
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-07-02 10:18:16 -0600

    Update models, schemas and controllers to handle new user attributes phone and org

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   validates :firstname, presence: true
   validates :lastname, presence: true
+  validates :jobtitle, presence: true
+  validates :organization, presence: true
+  validates :phone, presence: true
+  validates :phone, length: { is: 12 }
   validates :email, presence: true
   validates :email, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
