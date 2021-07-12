aea6fffe53b3cd61b9ab60ae8762fd8db8167a46
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-07-12 14:52:28 -0600

    Validate new contact data submitted from frontend

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,12 @@ class Contact < ApplicationRecord
   belongs_to :user
   scope :filter_by_user, -> (user_id) { where user_id: user_id }
   scope :filter_by_incident, -> (incident_id) { where incident_id: incident_id }
+
+  validates :name, presence:true
+  validates :jobtitle, presence:true
+  validates :organization, presence: true
+  validates :phone, presence: true
+  validates :phone, length: { is: 12 }
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
