a3b9ac0c4963fc541a47824a4c4162ef66346d49
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-07-19 13:08:13 -0600

    Change incident_role property from string to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_171758) do
+ActiveRecord::Schema.define(version: 2021_07_19_185432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_07_16_171758) do
   create_table "contacts", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.integer "incident_parent"
-    t.string "incident_role"
+    t.integer "incident_role"
     t.string "contact_type"
     t.string "name"
     t.string "jobtitle"
